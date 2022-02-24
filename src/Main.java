7aa28f1131cd0bdc1808551855b748be99b894c5
Bob <bob@example.com>
2022-02-24 23:11:44 +0300

    Option 3

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        MyClass myClass = new MyClass();
-        System.out.println(myClass.pipe());
+        JavaDFS graph = new JavaDFS(6);
+        graph.addEdge(0, 1);
+        graph.addEdge(0, 2);
+        graph.addEdge(1, 0);
+        graph.addEdge(1, 3);
+        graph.addEdge(2, 0);
+        graph.addEdge(2, 3);
+        graph.addEdge(3, 4);
+        graph.addEdge(3, 5);
+        graph.addEdge(4, 3);
+        graph.addEdge(5, 3);
+
+        System.out.println("Following is the Depth First Traversal");
+        graph.DFS(0);
     }
 }
